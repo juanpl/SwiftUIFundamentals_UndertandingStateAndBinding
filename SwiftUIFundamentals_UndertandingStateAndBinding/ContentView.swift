21796f7484008741d0d4848d995b20e108e781eb
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-10-07 09:24:07 -0500

    Initial Commit

---
+++ SwiftUIFundamentals_UndertandingStateAndBinding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFundamentals_UndertandingStateAndBinding
+//
+//  Created by Juan Pablo Lasprilla Correa on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFundamentals_UndertandingStateAndBinding/SwiftUIFundamentals_UndertandingStateAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentals_UndertandingStateAndBindingApp.swift
+//  SwiftUIFundamentals_UndertandingStateAndBinding
+//
+//  Created by Juan Pablo Lasprilla Correa on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentals_UndertandingStateAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
